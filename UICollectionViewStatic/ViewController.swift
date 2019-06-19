04ba580021254d2402c5ef0deef74cf9ad3d469c
Artem <inste@icloud.com>
2019-06-19 20:31:12 +0300

    First commit

---
+++ UICollectionViewStatic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICollectionViewStatic
+//
+//  Created by Артём Кармазь on 6/19/19.
+//  Copyright © 2019 Артём Кармазь. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

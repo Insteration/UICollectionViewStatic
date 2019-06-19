46af801c3b1e48135600931506178e2afe38c685
Artem <inste@icloud.com>
2019-06-19 20:40:48 +0300

    Third commit

--- UICollectionViewStatic/ViewController.swift
+++ UICollectionViewStatic/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     
     private let cellIds = ["Purple Cell", "Green Cell", "Blue Cell", "Red Cell"]
     private var cellSize = Array(repeating: CGSize(width: 170, height: 80), count: 4)
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,3 +21,14 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cellIds.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+    }
+    
+    
+}
